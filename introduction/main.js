67298fb66f48e4f487039d0c5d9e09042d6d52a0
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-06 16:33:14 +0800

    added explanation

---
+++ introduction/main.js
@@ -0,0 +1,8 @@
+console.log("1")
+console.log("2")
+console.log("3")
+console.log("4")
+console.log("5")
+
+//when you see the output you will observe that it will execute in order, starting from 1 to 5
+//this behaviour is not always ideal, hence we do asynchronus programming
