4456533f803cc7c1313bf5648f582a31c32de52d
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-07 09:45:33 +0800

    function1

--- callback/main.js
+++ callback/main.js
@@ -1,1 +1,28 @@
 //callback = 
+
+
+function task1(){
+setTimeout(() => {
+    console.log("1")
+}, 1000); 
+}
+
+setTimeout(() => {
+    console.log("1")
+}, 1000);
+
+setTimeout(() => {
+    console.log("2")
+}, 4000);
+
+setTimeout(() => {
+    console.log("3")
+}, 5000);
+
+setTimeout(() => {
+    console.log("4")
+}, 3000);
+
+setTimeout(() => {
+    console.log("5")
+}, 2000);
