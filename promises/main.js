3b0d6e0cfc1d3d457beb7e03808a5a9ce972d488
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-09 20:44:35 +0800

    promises def

--- promises/main.js
+++ promises/main.js
@@ -1,4 +1,11 @@
-//callback hell = can be messy to manage as you keep adding more functions
+// A promise is an assurance or guarantee that something will happen in  
+// the future. A person can promise another person a specific outcome or  
+// result. A Promise is an object that holds the future value of an async  
+// operation.  
+
+// Example: Requesting some data from a server, the promise, promises us  
+// to get that data which we can use in future.
+
 
 const showMessage = (callback) => {
     console.log(callback)
