e522578db7b49b29f5027f7d34a9e7fd38b6f3bd
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-11 20:59:50 +0800

    thenCatchCommonMistakes

---
+++ thenCatchCommonMistakes/main.js
@@ -0,0 +1,67 @@
+// A promise is an assurance or guarantee that something will happen in  
+// the future. A person can promise another person a specific outcome or  
+// result. A Promise is an object that holds the future value of an async  
+// operation.  
+
+// Example: Requesting some data from a server, the promise, promises us  
+// to get that data which we can use in future.
+
+//three states of promise:
+//pending = waiting
+//fulfilled = resolved
+//rejected = error
+
+// const promise = new Promise((resolve, reject) => {
+//     const bool = true;
+//     //const bool = false;
+
+//     if(bool){
+//         resolve('get booled')
+//     }else{
+//         reject('not booled')
+//     }
+// })
+
+
+const promise = new Promise((resolve, reject) => {
+  //  const randomNum = Math.floor(Math.random() * 10);
+
+  //  setTimeout(() => {
+  //     if(randomNum < 4){
+  //       resolve("resolved")
+  //     }else{
+  //       reject("rejected")
+  //     }
+  //  }, 2000);
+  resolve("well done Promise 1 is resolved")
+})
+
+
+const promise2 = new Promise((resolve, reject) => {
+  resolve("well done Promise 2 is resolved")
+})
+
+const promise3 = new Promise((resolve, reject) => {
+  reject("well done Promise 3 is rejected")
+})
+
+// promise.then((value) => {
+//   console.log(value)
+// }).catch((error) => {
+//   console.log(error)
+// })
+
+//chaining promises
+promise
+.then((value) => {
+  console.log(value)
+  return promise2
+})
+.then((value) => {
+  console.log(value)
+  return promise3
+}).catch((error) => {
+  console.log(error)
+})
+
+//much better writing instead of callback function as we run into callback hell
