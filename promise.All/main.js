2156b8da40cc186612e8eb39844e773a4b167eb0
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-12 21:04:56 +0800

    promise 2

--- promise.All/main.js
+++ promise.All/main.js
@@ -2,8 +2,14 @@
 //promse.all() = useful when we got a lot of promises that we want to fire off
 
 const promise1 = new promise1((resolve, reject) => {
-
   setTimeout(() => {
     resolve("promise 1 resolved")
+  }, 2000);
+});
+
+const promise2 = new promise1((resolve, reject) => {
+
+  setTimeout(() => {
+    resolve("promise 2 resolved")
   }, 2000);
 })
