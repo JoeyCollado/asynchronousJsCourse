16d207eac065c4a49457bce23578bab530cc4f4e
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-12 21:04:23 +0800

    promise 1

--- promise.All/main.js
+++ promise.All/main.js
@@ -1,73 +1,9 @@
-// A promise is an assurance or guarantee that something will happen in  
-// the future. A person can promise another person a specific outcome or  
-// result. A Promise is an object that holds the future value of an async  
-// operation.  
 
-// Example: Requesting some data from a server, the promise, promises us  
-// to get that data which we can use in future.
+//promse.all() = useful when we got a lot of promises that we want to fire off
 
-//three states of promise:
-//pending = waiting
-//fulfilled = resolved
-//rejected = error
+const promise1 = new promise1((resolve, reject) => {
 
-// const promise = new Promise((resolve, reject) => {
-//     const bool = true;
-//     //const bool = false;
-
-//     if(bool){
-//         resolve('get booled')
-//     }else{
-//         reject('not booled')
-//     }
-// })
-
-
-const promise = new Promise((resolve, reject) => {
-  //  const randomNum = Math.floor(Math.random() * 10);
-
-  //  setTimeout(() => {
-  //     if(randomNum < 4){
-  //       resolve("resolved")
-  //     }else{
-  //       reject("rejected")
-  //     }
-  //  }, 2000);
-  resolve("well done Promise 1 is resolved")
+  setTimeout(() => {
+    resolve("promise 1 resolved")
+  }, 2000);
 })
-
-
-const promise2 = new Promise((resolve, reject) => {
-  resolve("well done Promise 2 is resolved")
-})
-
-const promise3 = new Promise((resolve, reject) => {
-  reject("well done Promise 3 is rejected")
-})
-
-// promise.then((value) => {
-//   console.log(value)
-// }).catch((error) => {
-//   console.log(error)
-// })
-
-//bad practices, nesting promises kinda like callback hell
-promise
-.then((value) => {
-  console.log(value)
-  promise2.then((data) => {
-    console.log(data);
-
-  promise3.then((data) => {
-    console.log(data);
-  }).catch((error) => {
-    console.log(error)
-  })
-  })
-})
-.catch((error) => {
-  console.log(error)
-})
-
-
-//much better writing instead of callback function as we run into callback hell
