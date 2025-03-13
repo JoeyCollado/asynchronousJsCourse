ea736bf8335f6b77036813d502cafde1a2889cb8
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-13 18:24:48 +0800

    Async func 2

--- asyncAwait/main.js
+++ asyncAwait/main.js
@@ -13,3 +13,45 @@ const preHeatOven = () => {
      }, 1000)
   })
 }
+
+const addSugar = () => {
+  return new Promise((resolve, reject) => {
+     setTimeout(() => {
+      const addSugar = true;
+
+      if(addSugar){
+        resolve("added sugar")
+      }else{
+        reject("failed task")
+      }
+     }, 1000)
+  })
+}
+
+const preHeatOven = () => {
+  return new Promise((resolve, reject) => {
+     setTimeout(() => {
+      const preHeatOven = true;
+
+      if(preHeatOven){
+        resolve("preheat oven to 180 deg")
+      }else{
+        reject("failed task")
+      }
+     }, 1000)
+  })
+}
+
+const preHeatOven = () => {
+  return new Promise((resolve, reject) => {
+     setTimeout(() => {
+      const preHeatOven = true;
+
+      if(preHeatOven){
+        resolve("preheat oven to 180 deg")
+      }else{
+        reject("failed task")
+      }
+     }, 1000)
+  })
+}
