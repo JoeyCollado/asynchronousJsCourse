487a2f2fb0374bbc87c2d99cb161ec6017b6d9f5
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-14 22:14:22 +0800

    Basic put method

--- fetchApi/main.js
+++ fetchApi/main.js
@@ -1,13 +1,33 @@
 //fetch = allow us to make http request to web server = {'<URL>', {Method property and request we want}} 
+//post = get data
+//put = update existing product
 
-
+/*
 fetch('https://dummyjson.com/products/add', {
-  method: 'POST',
-  headers: {
+  method: 'POST', //method to use
+  headers: { //content type we want to create
     'Content-Type': 'application/json'
   },
-  body: JSON.stringify({
+  body: JSON.stringify({ //include data we want
     description: "Iphone 19",
+    price: "1000",
+    rating: "9/10"
+
+  })
+})
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.log(error))
+*/
+
+fetch('https://dummyjson.com/products/1', {
+  method: 'PUT', //method to use
+  headers: { //content type we want to create
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ //include data we want
+    title: "iphone 19",
+    description: "Changed to iphone 19",
     price: "1000",
     rating: "9/10"
 
