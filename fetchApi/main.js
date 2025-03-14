18ce443ed245e164d18f0f563069951869f64ec2
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-14 22:11:37 +0800

    basic post method

--- fetchApi/main.js
+++ fetchApi/main.js
@@ -1,7 +1,18 @@
-//fetch = allow us to make http request to web server = {'<URL>', {}} 
+//fetch = allow us to make http request to web server = {'<URL>', {Method property and request we want}} 
 
 
-fetch('https://dummyjson.com/products/1', {})
+fetch('https://dummyjson.com/products/add', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    description: "Iphone 19",
+    price: "1000",
+    rating: "9/10"
+
+  })
+})
 .then(response => response.json())
 .then(data => console.log(data))
 .catch(error => console.log(error))
