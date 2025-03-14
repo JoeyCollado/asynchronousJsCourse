88974864e2335836e45bfd8906444eaf1c469118
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-14 22:06:09 +0800

    console response

--- fetchApi/main.js
+++ fetchApi/main.js
@@ -1,1 +1,6 @@
 //fetch = allow us to make http request to web server = {'<URL>', {}} 
+
+
+fetch('https://dummyjson.com/products/1', {})
+.then(response => console.log(response))
+.catch(error => console.log(error))
