ff80f33fccb4189085f3e89e0d089d7d3fbd068e
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-17 16:20:43 +0800

    basic js

--- weatherApi/main.js
+++ weatherApi/main.js
@@ -1,1 +1,22 @@
-//
+// 154ce9c4290971119d1e19bac1a4e4ad
+
+const date = document.getElementById('date');
+const city = document.getElementById('city');
+const temp = document.getElementById('temp')
+const tempImg = document.getElementById('tempImg')
+const description = document.getElementById('description')
+const tempMax = document.getElementById('tempMax')
+const tempMin = document.getElementById('tempMin')
+
+const months = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+
+let dateObj = new Date()
+let month = months[date.getUTCMonth()]; //will equal to what month it is targeting the months array
+let day = dateObj.getUTCDate() - 1; //get day
+let year = dateObj.getUTCFullYear();
+
+//displaying in dom
+date.innerHTML = `${month} ${day}, ${year}`;
