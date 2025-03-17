a4d2ac439133e8886d19ac8f6672fd2edef05e04
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-17 16:25:19 +0800

    url fetched

--- weatherApi/main.js
+++ weatherApi/main.js
@@ -14,9 +14,26 @@ const months = [
   ];
 
 let dateObj = new Date()
-let month = months[date.getUTCMonth()]; //will equal to what month it is targeting the months array
+let month = months[dateObj.getUTCMonth()]; //will equal to what month it is targeting the months array
 let day = dateObj.getUTCDate() - 1; //get day
 let year = dateObj.getUTCFullYear();
 
 //displaying in dom
 date.innerHTML = `${month} ${day}, ${year}`;
+
+const app = document.getElementById('app')
+
+const getWeather = async () => {
+    try {
+        const weatherDataFetch = await fetch(
+            'https://api.openweathermap.org/data/2.5/weather?q=London&appid=&units=metric',
+
+            headers: {
+                Accept: "application/json"
+            }
+        );
+    }
+    catch(error){
+        console.log(error)
+    }
+}
